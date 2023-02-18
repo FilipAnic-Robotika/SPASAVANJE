0332cde3c35e5f07fc18e811827de47543700812
FilipAnic-Robotika <125739881+FilipAnic-Robotika@users.noreply.github.com>
2023-02-18 07:56:05 -0500

    Update radionica.h

--- FUNKCIJE/radionica.h
+++ FUNKCIJE/radionica.h
@@ -1,1 +1,23 @@
-a
+ #pragma once
+ #include "helper.h"
+
+
+void RobotLine::radionica()
+{
+pratiLinijuBrzo();
+if(front() < 100){
+
+go((-50), 50);
+  delayMs(1000);
+  go(50, 50);
+  delayMs(2000);
+  go(50, (-50));
+  delayMs(1000);
+  go(50, 50);
+  delayMs(2000);
+  go(50, (-50));
+  delayMs(1000);
+  go(50, 50);
+  delayMs(2000);
+}
+}
