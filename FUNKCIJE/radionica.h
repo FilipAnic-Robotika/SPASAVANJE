cd099ba0ed6651ce4cc27d7313f85e2a21617278
FilipAnic-Robotika <125739881+FilipAnic-Robotika@users.noreply.github.com>
2023-03-25 14:01:31 +0100

    Update radionica.h

--- FUNKCIJE/radionica.h
+++ FUNKCIJE/radionica.h
@@ -3,38 +3,41 @@
 
 
 void RobotLine::radionica(){
-  static int t = 0;
-if(t==2 and line(8)){
-go (50, 50);
+static int t = 0;
+if ((t==0) and line(7) and line(3) and pitch()<5 and pitch()>-5){
+go(-90, 90);
+  delayMs(500);
+  t=t+1;
+}
+if(t==1 and line(6) and line(2)){
+go (40, 40);
   delayMs(500);
   t=t+1;
 
 }
-if(t==3 and line(1) and line(7)){
+
+if(t==2 and line(1) and line(7)){
 go(50, -50);
   delayMs(2000);
   t=t+1;
 }
-if(t==4 and line(0)){
+if(t==3 and line(0)){
 go(50, -50);
   delayMs(1000);
   t=t+1;
 }
-if (line(2) and line(6) and pitch()<3 and pitch()>-1 and (t==0 or t==1)){
-go(-90, 90);
-  delayMs(500);
-  t = t+1;
-}
+
 if (pitch() < -10 and line(1) and line(7)) {
     go(50, 50);
     delayMs(500);
 }
-else if (pitch()<4 and pitch()>-4 and line(2) and line(6) and t==5) {
+else if (pitch()<4 and pitch()>-4 and line(2) and line(6) and t==4) {
     go(50, 50);
     delayMs(1000);
 }
 
-if (front()<150 and front()>100 and t==2) {
+if (front()<160 and front()>150 and t==1) {
+  if (front()<160 and front()>150 and t==1) {
     go(50, (-50));
     delayMs(800);
     go(50, 50);
@@ -42,15 +45,16 @@ if (front()<150 and front()>100 and t==2) {
     go((-50), 50);
     delayMs(800);
     go(50, 50);
-    delayMs(1000);
-    go(50, (-50));
+    delayMs(1500);
+    go(-50, (50));
     delayMs(800);
     go(50, 50);
-    delayMs(1000);
-    go((-50), 50);
+    delayMs(900);
+    go((50), -50);
     delayMs(800);
-
+  }
 }
+
 
 else{
   pratiLiniju();
