2e01d853212ac5445efff06bb6e0e8897f843d3c
FilipAnic-Robotika <125739881+FilipAnic-Robotika@users.noreply.github.com>
2023-04-22 13:53:18 +0200

    Update radionica.h

--- FUNKCIJE/radionica.h
+++ FUNKCIJE/radionica.h
@@ -1,43 +1,52 @@
- #pragma once
- #include "helper.h"
+#pragma once
+#include "helper.h"
 
 
-void RobotLine::radionica(){
-static int t = 0;
-if ((t==0) and line(7) and line(3) and pitch()<5 and pitch()>-5){
-go(-90, 90);
-  delayMs(500);
-  t=t+1;
-}
-if(t==1 and line(6) and line(2)){
-go (40, 40);
-  delayMs(500);
-  t=t+1;
+void RobotLine::radionica() {
+  static int t = 0;
+  static int linija = 0;
+   static int linija2 = 0;
+  
+  static int cunj = 0;
+  if ((t == 0) and line(7) and line(3) and pitch()<5 and pitch()> -5) {
+    go(-90, 90);
+    delayMs(500);
+    t = t + 1;
+  }
+  if (t == 1 and line(6) and line(2)) {
+    go(40, 40);
+    delayMs(500);
+    t = t + 1;
 
-}
+  }
 
-if(t==2 and line(1) and line(7)){
-go(50, -50);
-  delayMs(2000);
-  t=t+1;
-}
-if(t==3 and line(0)){
-go(50, -50);
-  delayMs(1000);
-  t=t+1;
-}
+  if (t == 2 and line(1) and line(7)) {
+    go(50, -50);
+    delayMs(2000);
+    t = t + 1;
+  }
+  if (t == 3 and line(0)) {
+    go(50, -50);
+    delayMs(1000);
+    t = t + 1;
+  }
 
-if (pitch() < -10 and line(1) and line(7)) {
+  if (pitch() < -10 and line(1) and line(7)) {
     go(50, 50);
     delayMs(500);
-}
-else if (pitch()<4 and pitch()>-4 and line(2) and line(6) and t==4) {
+  }
+  else if (pitch()<10 and line(1) and line(7) and t == 4) {
     go(50, 50);
     delayMs(1000);
-}
+    armOpen();
+    linija2 = 1;
 
-if (front()<160 and front()>150 and t==1) {
-  if (front()<160 and front()>150 and t==1) {
+  }
+  if (not lineAny() and cunj == 0) {
+    linija = linija + 1;
+  }
+  if (front()<150 and front()>140 and t == 1 and linija > 100 and cunj == 0) {
+
     go(50, (-50));
     delayMs(800);
     go(50, 50);
@@ -52,17 +61,18 @@ if (front()<160 and front()>150 and t==1) {
     delayMs(900);
     go((50), -50);
     delayMs(800);
+    cunj = 1;
   }
-}
+  if (linija2==1){
+   pratiDesniZid();
+  }
 
+  else {
+    pratiLiniju();
+  }
 
-else{
-  pratiLiniju();
-}
-
-  
-char simbol[10 + sizeof(char)];
-  sprintf(simbol,"%d",t);
+  char simbol[10 + sizeof(char)];
+  sprintf(simbol, "%d", linija);
   display(simbol);
 }
 
