c9f451578491bb0ff6485a8b3400c87c5ca98e95
FilipAnic-Robotika <125739881+FilipAnic-Robotika@users.noreply.github.com>
2023-06-10 12:12:40 +0200

    Update radionica.h

--- FUNKCIJE/radionica.h
+++ FUNKCIJE/radionica.h
@@ -4,23 +4,23 @@
 
 void RobotLine::radionica() {
   
-    static int t=0;
-    static int h=0;
- 
-  if (line(7) and line(1)) {
+  static int r = 0;
+  static int p = 0;
+
+  if (line(7) and line(1) and r == 0) {
+    p++;
+    go(30, 30);
+    delayMs(300);
     go(-90, 90);
     delayMs(600);
-    go(90, 90);
-    delayMs(1000);
-    
+
   }
-  if(not lineAny()){
-    t=t+1;
-  }
-  if(not lineAny() and t > 5){
-    h=h+1;
-  }  
-  else{
-    pratiLiniju();
-  }
-}
+
+  lineFollow();
+
+}  
+/*
+  char simbol[10 + sizeof(char)];
+  sprintf(simbol, "%d", ms);
+  display (simbol);/*
+  
